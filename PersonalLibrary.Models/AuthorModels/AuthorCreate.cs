82fa548d8e1f88480b37e89dcb20ae8923c10bb3
dylandunn <dylandunn54@gmail.com>
2021-09-14 19:31:08 -0400

    Models/Services Commit

---
+++ PersonalLibrary.Models/AuthorModels/AuthorCreate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalLibrary.Models.AuthorModels
+{
+    public class AuthorCreate
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
