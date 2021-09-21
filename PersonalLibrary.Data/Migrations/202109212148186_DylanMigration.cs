5f0ae65060d4b9775cb22aa40eb1f99a1f00771d
dylandunn <dylandunn54@gmail.com>
2021-09-21 17:51:23 -0400

    Migration Fixes

---
+++ PersonalLibrary.Data/Migrations/202109212148186_DylanMigration.cs
@@ -0,0 +1,18 @@
+namespace PersonalLibrary.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class DylanMigration : DbMigration
+    {
+        public override void Up()
+        {
+            
+        }
+        
+        public override void Down()
+        {
+           
+        }
+    }
+}
